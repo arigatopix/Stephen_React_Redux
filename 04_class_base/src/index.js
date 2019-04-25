35e38b52c6b2588fffdc610df6994d39ccba9a72
arigatopix <teeruch.k@gmail.com>
2019-04-25 15:09:02 +0700

    :fire: use state instead of constructor()

--- 04_class_base/src/index.js
+++ 04_class_base/src/index.js
@@ -3,50 +3,48 @@ import ReactDOM from 'react-dom';
 
 class App extends Component {
 
+/* 
   // Special function (not require by react)
   constructor(props) {
     super(props); // ใช้ super เพราะว่าเรียกใช้ parent จาก Component
 
-    // ! init state ตอน render page ใช้ state() method เป็น special function ของ Component
+    // ! Constructor มีหน้าที่เดียวคือ init state ตอน render page ใช้ state() method เป็น special function ของ Component
     // * THIS IS THE ONLY TIME we do direct assignment to this.state
     this.state = {
       lat : null,
       long : null,
       errorMessage : ''
     };
+  }
+*/
 
+  // * Alternate Init state ไม่ต้องใช้ constructor แต่จะใช้ Babel ในการ transpli
+   state = { lat : null, errorMessage : '' };
 
-    // run function แล้วให้ return this.setState() ซึ่ง setState เป็น special property ของ Component
+  // Lifecycle component ถูกเรียกอัตโนมัติ
+  componentDidMount() {
+    // แสดงตอนแรกสุดที่ component แสดง
+
+    // * run function แล้วให้ return this.setState() ซึ่ง setState เป็น special property ของ Component
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-
-        // * we called setState ส่งกลับเป็น object ทั้งก้อน เพื่อไป update state ต้องใช้ setState
-        this.setState({
-          lat : position.coords.latitude
-        });
-
-        // we did not!! เพราะ state ได้แค่ค่าเดียว อย่างเงดียว
-        // this.state.lat = position.coords.latitude;
-      },
+      position => this.setState({ lat : position.coords.latitude }),
       err => this.setState({ errorMessage : err.message })
-      // อย่าลืม Handle error ด้วย จะได้บอก user ได้ว่าเกิดอะไรขึ้น
-      // * setState errorMessage จะเปลี่ยนแค่อันเดียว property อื่นไม่เปลี่ยน (lat คงสภาพเดิม)
     );
   }
 
   // React says we have to define render!! return jsx
   render() {
 
-      if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error : {this.state.errorMessage}</div>
-      } else if (!this.state.errorMessage && this.state.lat) {
-        return <div>Latitude : {this.state.lat}</div>
-      }
+    // Show err,
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error : {this.state.errorMessage}</div>
+      // NO error and show Latitude
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude : {this.state.lat}</div>
+    }
 
-      return <div>Loading!</div>
-
+    return <div>Loading!</div>
     // ระวัง ; อย่าใส่ด้านหลัง JSX
-
   }
 }
 
