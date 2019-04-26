fc363f3ee48ef6e87d0bd958981922ce532b5c4e
arigatopix <teeruch.k@gmail.com>
2019-04-26 15:01:19 +0700

    :fire: Understandding this IN JS

--- 07_pics_project/src/components/SearchBar.js
+++ 07_pics_project/src/components/SearchBar.js
@@ -3,20 +3,32 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  onFormSubmit = (e) => { 
+    // ป้องกันไม่ให้ submit ข้อมูล และ reload page
+    e.preventDefault() 
+
+    console.log(this.state.term); 
+    // this นี้เป็นของ global scope ไปแล้ว เพราะเป็น function ซ้อนกันสองครั้ง
+    
+    // onFormSubmit : function() {
+    //     console.log(this.state.term) ---- > this ของ global scope
+    // } 
+  }
+
   render() {
-    console.log(this.state.term);
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              onChange={e => this.setState({ term: e.target.value })} 
             />
           </div>
         </form>
+        <h1>{this.state.term}</h1>
       </div>
     );
   }
@@ -37,4 +49,17 @@ export default SearchBar;
  * --
  * ตั้งชื่อ function call
  * - เริ่มที่ on แล้วต่อด้วย element และ change คือชื่อ event ที่ใช้ function call
+ * ---
+ * - INPUT รับค่าจาก onChange={e => this.setState({ term: e.target.value })}
+ * - INPUT แสดงค่าที่ได้รับ (rerender) แสดงผลด้วย value={this.state.term}
+ * - ให้เข้าใจว่า React เก็บค่า จาก onChange ที่พิมพ์มาไว้ใน this.state ส่วนใน DOM คอยรับค่า (onChnage) และแสดงค่า (value)
+ * ------
+ * - form behavior ปกติจะต้องมี submit ส่งข้อมูลให้ backend พร้อมกับ reload page 
+ * - โดยปกติจะใช้ this ได้โดยเรียก variable ไม่ใช่เรียก method
+ * - ระวังเรื่อง this. เพราะคนละ scope (เช่น this.onFormSubmit this นี้คือ window) 
+ * - โดย this ปกติ เป็น reference ของ class SearchBar (Component) ใช้กับ state, render, onFormSubmit ได้เพราะเป็น method ที่ instance จาก class 
+ * - วิธีแก้คือใช้ Arrow Function - ใช้ได้ 2 ที่คือ
+ *  - ใช้ Arrorw ใน class ปกติใช้ตรงนี้
+ *  - ใช้ Arrow fn ที่ JSX onSubmit={(e) => this.onFormSubmit(e)}
+ * - 
  */
