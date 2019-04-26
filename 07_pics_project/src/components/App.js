9afb832441e09289f36ef2174b604aeb9b7a490e
arigatopix <teeruch.k@gmail.com>
2019-04-26 16:28:29 +0700

    :rocket: Request Result axios

--- 07_pics_project/src/components/App.js
+++ 07_pics_project/src/components/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos/',{
+      params : { query : term },
+      headers : {
+        Authorization: 'Client-ID d2f6f5c0dec5b5d42917996d62c9b368f0bc771666660a2919542aeefc0099ac'
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
+}
 
 export default App;
+
+/**
+ * onSubmit={this.onSearchSubmit} จะใช้ได้แค่ form tag ดังนั้นในหน้านี้จะใส่ชื่อ props อะไรก็ได้
+ * - props จาก child จะส่งผ่านข้อมูลตรงนี้เพื่อเรียก function
+ * --- axios
+ * - param : เป็นตัว query
+ * - headers บอก api key
+  */
--- 07_pics_project/src/components/SearchBar.js
+++ 07_pics_project/src/components/SearchBar.js
@@ -4,15 +4,9 @@ class SearchBar extends React.Component {
   state = { term: '' };
 
   onFormSubmit = (e) => { 
-    // ป้องกันไม่ให้ submit ข้อมูล และ reload page
     e.preventDefault() 
 
-    console.log(this.state.term); 
-    // this นี้เป็นของ global scope ไปแล้ว เพราะเป็น function ซ้อนกันสองครั้ง
-    
-    // onFormSubmit : function() {
-    //     console.log(this.state.term) ---- > this ของ global scope
-    // } 
+    this.props.onSubmit(this.state.term); // this.props.onSubmit คือเรียกใช้ props จาก parent ส่งข้อมูล this.state.term ให้ App หรือพูดอีกทางคือส่งข้อมูลที่ user input ให้ App component รู้
   }
 
   render() {
@@ -28,7 +22,6 @@ class SearchBar extends React.Component {
             />
           </div>
         </form>
-        <h1>{this.state.term}</h1>
       </div>
     );
   }
@@ -37,29 +30,5 @@ class SearchBar extends React.Component {
 export default SearchBar;
 
 /**
- * NOTE:
- * - property รับ event handle user input onChange={this.onInputChange} ไม่ใช้ onInputChange()
- * - จะเรียกเมื่อ <input> ได้รับค่า แล้ว callback funtion
- * บางทีอาจจะเห็น onChange={(event) => console.log(event.target.value) }
- * --
- * Event
- * - onClick รับปุ่ม click
- * - onChange ใช้กับ input text
- * - onSubmit ใช้กับปุ่ม button
- * --
- * ตั้งชื่อ function call
- * - เริ่มที่ on แล้วต่อด้วย element และ change คือชื่อ event ที่ใช้ function call
- * ---
- * - INPUT รับค่าจาก onChange={e => this.setState({ term: e.target.value })}
- * - INPUT แสดงค่าที่ได้รับ (rerender) แสดงผลด้วย value={this.state.term}
- * - ให้เข้าใจว่า React เก็บค่า จาก onChange ที่พิมพ์มาไว้ใน this.state ส่วนใน DOM คอยรับค่า (onChnage) และแสดงค่า (value)
- * ------
- * - form behavior ปกติจะต้องมี submit ส่งข้อมูลให้ backend พร้อมกับ reload page 
- * - โดยปกติจะใช้ this ได้โดยเรียก variable ไม่ใช่เรียก method
- * - ระวังเรื่อง this. เพราะคนละ scope (เช่น this.onFormSubmit this นี้คือ window) 
- * - โดย this ปกติ เป็น reference ของ class SearchBar (Component) ใช้กับ state, render, onFormSubmit ได้เพราะเป็น method ที่ instance จาก class 
- * - วิธีแก้คือใช้ Arrow Function - ใช้ได้ 2 ที่คือ
- *  - ใช้ Arrorw ใน class ปกติใช้ตรงนี้
- *  - ใช้ Arrow fn ที่ JSX onSubmit={(e) => this.onFormSubmit(e)}
- * - 
- */
+ * 
+  */
