ce6f6a3c0d668d1b72dd01e3594d3fe5e7c2e4bb
arigatopix <teeruch.k@gmail.com>
2019-04-30 10:05:23 +0700

    :pushpin: add SearchBar component

---
+++ 11_video_project/src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+  state = { term : '' };
+
+  onInputChange = (event) => {
+    // รับค่า input
+    this.setState({ term : event.target.value });
+  };
+
+  onFormSubmit = event => {
+    // * ป้องกัน submin แล้ว reload page
+    event.preventDefault();
+
+    // clear input หลัง submit
+    this.setState({ term : '' });
+
+    // TODO : Make sure we call callback form parent พอ submit แล้วไปเรียก API
+    console.log(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" value={ this.state.term } onChange={ this.onInputChange }/>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
+
+/**
+ * NOTE
+ * - onChange เป็น special name จะ callback funtion
+ * - onFormSubmit เป็น special เหมือนกัน อย่าลืม e.preventDefault ป้องกันการ reload
+  */
