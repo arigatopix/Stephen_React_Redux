260c05c611a3cad8768aaa71b47af6cc5012835c
arigatopix <teeruch.k@gmail.com>
2019-05-03 15:40:46 +0700

    :pushpin:config redux-thunk (action)

--- 14_blog/src/actions/index.js
+++ 14_blog/src/actions/index.js
@@ -1,14 +1,15 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-  // fetch data from json placeholder / posts
-  //! bad approch !!! break the rule  ของ redux ตอนตอบเป็น plain object
-  const promise = jsonPlaceholder.get('/posts');
+  return async function(dispatch,getState) {
+    // ใช้ redux-thunk จะ return เป็น function จนกว่า asyn await เป็น object plain ก็จะ run dispatch แบบ manaul
 
-  return {
-    type : 'FETCH_POSTS',
-    payload : promise
-  };
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload : response })
+    // เรียก dispatch เมื่อ async ส่งข้อมูลกลับมา คำถามคือทำไมไม่มีปัญหา ??
+    // ได้เพราะ เมื่อไหร่ที่เรียก action จะ return request object แทนที่จะเป็น action object (ปกติ thunk ไม่สนอยู่แล้วว่าจะเป็นอะไร) หลังจากนั้น พอได้ข้อมูลก็จะเรียก dispatch 
+  }
 };
 
 /**
@@ -18,4 +19,13 @@ export const fetchPosts = () => {
  *    - มี code บางส่วน return request object (ที่ไป fetch) เป็น js plain object จริง แต่ไม่ใช่ action พอ dispatch ปุ้บก็ error ต้องใช้ async ที่ middleware
  *    - วิธีแก้ตอนแรก เอา async await ออก จะได้ object เหมือนเดิม แต่ว่าจะ return promise object 
  *    - ซึ่งเอา async await มีผลตอนแสดงผล เพราะไม่มี function callback รอ data ที่ส่งมาจาก jsonplaceholder) ทำให้ reducers มองไม่เหน response
+ *    ----------------------
+ * Middleware
+ * - คือ Async Action creators โดยใช้ redux thunk
+ * - สามารถเรียก action เป็น object หรือ function ก็ได้
+ * - เรียก dispatch เพื่อสั่ง action ทำงานผ่าน reducers และ getStore เพื่อดู redux store เมื่อไหร่ก็ได้
+ * - หลังจาการอ request ส่งมา middleware จะเปลี่ยน function เป็น Action เพื่อเข้า dispatch ปกติ
+ * - ลองดู flow behind the scenes of redux thunk
+ * --------
+ * - เปลี่ยนโครงสร้าง return action object เป็น request object (function) ให้ middleware จัดการ
   */
--- 14_blog/src/index.js
+++ 14_blog/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 
 import App from './components/App';
 import reducers from './reducers';
 
+// Init redux thunk
+const store = createStore(reducers, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>, 
   document.querySelector('#root')
@@ -18,4 +22,8 @@ ReactDOM.render(
  * - import โครงสร้าง redux ใน Index.js ทั้งหมด
  *    - createStore(reducers) คือเก็บ state ไว้ใน Store ของ Redux
  *    - Provider เป็น component ติดต่อ React-Redux 
+ * ------
+ * Middleware
+ * - applyMiddleware มาจาก redux
+ * - thunk เป็น library ให้ redux ใช้ middleware
   */
