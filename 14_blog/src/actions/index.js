9383ca1a043441c00e7090400591f221f1681881
arigatopix <teeruch.k@gmail.com>
2019-05-03 11:45:41 +0700

    :pushpin: explain what happen action creators

--- 14_blog/src/actions/index.js
+++ 14_blog/src/actions/index.js
@@ -16,5 +16,6 @@ export const fetchPosts = () => {
  * - ไม่ใช่ object plain เพราะ 
  *    - ใช้ async await > transpile เป็น es 2015
  *    - มี code บางส่วน return request object (ที่ไป fetch) เป็น js plain object จริง แต่ไม่ใช่ action พอ dispatch ปุ้บก็ error ต้องใช้ async ที่ middleware
- *    - วิธีแก้ตอนแรก เอา async await ออก จะได้ object เหมือนเดิม แต่ว่าจะ return promise object (มีผลตอนแสดงผล เพราะไม่มี function callback รอ data ที่ส่งมาจาก jsonplaceholder)
+ *    - วิธีแก้ตอนแรก เอา async await ออก จะได้ object เหมือนเดิม แต่ว่าจะ return promise object 
+ *    - ซึ่งเอา async await มีผลตอนแสดงผล เพราะไม่มี function callback รอ data ที่ส่งมาจาก jsonplaceholder) ทำให้ reducers มองไม่เหน response
   */
