74932cb377b9028b048de003117f51c6c1e8e1cc
arigatopix <teeruch.k@gmail.com>
2019-05-03 11:45:12 +0700

    :pushpin: explain what happen action creators

--- 14_blog/src/actions/index.js
+++ 14_blog/src/actions/index.js
@@ -1,12 +1,20 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => {
   // fetch data from json placeholder / posts
   //! bad approch !!! break the rule  ของ redux ตอนตอบเป็น plain object
-  const response = await jsonPlaceholder.get('/posts');
+  const promise = jsonPlaceholder.get('/posts');
 
   return {
     type : 'FETCH_POSTS',
-    payload : response
+    payload : promise
   };
 };
+
+/**
+ * NOTE : 
+ * - ไม่ใช่ object plain เพราะ 
+ *    - ใช้ async await > transpile เป็น es 2015
+ *    - มี code บางส่วน return request object (ที่ไป fetch) เป็น js plain object จริง แต่ไม่ใช่ action พอ dispatch ปุ้บก็ error ต้องใช้ async ที่ middleware
+ *    - วิธีแก้ตอนแรก เอา async await ออก จะได้ object เหมือนเดิม แต่ว่าจะ return promise object (มีผลตอนแสดงผล เพราะไม่มี function callback รอ data ที่ส่งมาจาก jsonplaceholder)
+  */
