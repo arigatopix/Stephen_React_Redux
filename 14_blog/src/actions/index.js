3fbe891a61a44660f2901a971a47ea4083e2bee1
arigatopix <teeruch.k@gmail.com>
2019-05-03 10:55:44 +0700

    :pushpin: Action musbi plain object >> use custom middleware !!!

--- 14_blog/src/actions/index.js
+++ 14_blog/src/actions/index.js
@@ -1,5 +1,12 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+  // fetch data from json placeholder / posts
+  //! bad approch !!! break the rule  ของ redux ตอนตอบเป็น plain object
+  const response = await jsonPlaceholder.get('/posts');
+
   return {
-    type : 'FETCH_POSTS'
+    type : 'FETCH_POSTS',
+    payload : response
   };
 };
