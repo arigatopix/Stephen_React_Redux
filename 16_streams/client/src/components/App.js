f6e38b413cb0adb4ecf9e6cd74fc3783d0c79d99
arigatopix <teeruch.k@gmail.com>
2019-05-09 12:28:10 +0700

    :fire: use <a> tag

--- 16_streams/client/src/components/App.js
+++ 16_streams/client/src/components/App.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <a href="/pagetwo">Go to page Two (BAD!!!)</a>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <a href="/">Back to page One (BAD!!!)</a>
+      <button>Click Me</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
