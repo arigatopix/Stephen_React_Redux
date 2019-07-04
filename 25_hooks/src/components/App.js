be22fca47e459e33f1b8806df764986c5bd1cf55
arigatopix <teeruch.k@gmail.com>
2019-07-04 13:58:37 +0700

    init Hooks project

---
+++ 25_hooks/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return <div className="container">app</div>;
+};
+
+export default App;
+
+/**
+ * HOOKS w/ Functional Components สำหรับ shared logic between components
+ */
